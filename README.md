783504d8eaed70f7e9c3226e665cafc72a63bb24
Lawrence Li <lonor@live.com>
2020-09-08 12:00:07 +0800

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Solidot Fanfou Robot
 
-- todo
+【非官方机器人】每 10 分钟我就去 solidot 看看有没有新闻，然后将新的消息发到饭否。
--- api/new.js
+++
@@ -1,5 +0,0 @@
-module.exports = async (req, res) => {
-    // Respond with a JSON string of all users in the collection
-    console.log("test" + new Date());
-    res.status(200).json({ "test": "new" })
-  }
