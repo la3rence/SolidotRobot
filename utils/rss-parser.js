96af08170998ec5085468758c36510b44e41d71e
Lawrence Li <lonor@live.com>
2020-09-08 11:41:12 +0800

    change parser

--- utils/rss-parser.js
+++ utils/rss-parser.js
@@ -1,15 +1,13 @@
 const Parser = require('rss-parser');
 
 const parser = new Parser({
-    customFields: {
-        item: ['magnet'],
-    },
     headers: {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
     },
     requestOptions: {
         rejectUnauthorized: false
     },
+    timeout: 10000,
 });
 
 module.exports = parser;
