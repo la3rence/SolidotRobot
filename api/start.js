ee67d0802e66f167cee9d4114efc63c3cb5d96af
Lawrence Li <lonor@live.com>
2020-09-08 14:50:22 +0800

    remove koa

--- api/start.js
+++ api/start.js
@@ -1,7 +1,7 @@
 var doRss = require('../main');
 
 module.exports = async (req, res) => {
-    await doRss();
-    // Respond with a JSON string of all users in the collection
-    res.status(200).json({ "hello": "world" })
-  }
+  await doRss();
+  // Respond with a JSON string of all users in the collection
+  res.status(200).json({ "hello": "world" })
+}
--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-var db = require('./utils/db');
+const db = require('./utils/db');
 const parser = require('./utils/rss-parser');
 const fanfou = require('./utils/fanfou');
-// const schedule = require('node-schedule');
 const tableName = 'solidot';
 
 async function doRss() {
-    console.log("开始解析 RSS")
     const feed = await parser.parseURL('http://www.solidot.org/index.rss');
     await Promise.all(
         feed.items.map(async article => {
@@ -20,11 +18,11 @@ async function doRss() {
         }));
 }
 
+module.exports = doRss;
+
+// const schedule = require('node-schedule');
 // function main() {
-//     console.log("进入 main()")
-//     schedule.scheduleJob('0 * * * * *', async () => {
-//         console.log('时间: ' + new Date());
+//     schedule.scheduleJob('*/1 * * * * *', async () => {
 //         await doRss();
 //     });
 // }
-module.exports = doRss;
