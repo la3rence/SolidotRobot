0216647fa1d5342201ca7c5d7c45b8c00d37e522
Lawrence Li <lonor@live.com>
2020-09-16 18:06:18 +0800

    update cron and user agent

--- utils/rss-parser.js
+++ utils/rss-parser.js
@@ -2,7 +2,7 @@ const Parser = require('rss-parser');
 
 const parser = new Parser({
     headers: {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36',
     },
     requestOptions: {
         rejectUnauthorized: false
