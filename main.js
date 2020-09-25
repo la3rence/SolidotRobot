f301f48586c3f6bb2757172696cb50cdaee66d2c
Lawrence Li <lonor@live.com>
2020-09-26 02:13:16 +0800

    add comment

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const db = require('./utils/db');
 const parser = require('./utils/rss-parser');
 const fanfou = require('./utils/fanfou');
 const URL = require('url');
-const { isRegExp } = require('util');
 const tableName = 'solidot';
 
 function getSidFromLink(link) {
@@ -14,6 +13,7 @@ async function doRss() {
     let list = [];
     console.log("开始获取 RSS 资源");
     const feed = await parser.parseURL('https://www.solidot.org/index.rss');
+    // todo: 判断是否有新文章后再调用 fanfou api 以优化，这里明显每次都要登录。
     let fanfouClient = await fanfou.authFan();
     await Promise.all(
         feed.items.map(async article => {
@@ -27,14 +27,12 @@ async function doRss() {
                 if (beforeLinkCount > 0) {
                     // console.log(`删除旧链接 ${beforeLink}`);
                     await db.deleteOne(tableName, { link: beforeLink });
-                } else {
-                    // console.log(`旧链接 ${beforeLink} 已被删除`);
-                }
+                } 
             } else {
                 try {
                     console.log(`执行发布: ${article.title}`);
-                    // todo：敏感词
-                    let cleanTitle = article.title.replace(new RegExp("特朗普", "gm"), "Trump");
+                    // todo：敏感词收集
+                    let cleanTitle = article.title.replace(new RegExp("特朗普", "gm"), " Trump ");
                     resFromFanfou = await fanfouClient.post('/statuses/update', { status: `${cleanTitle} ${article.link}` });
                     await db.insertOne(tableName, { link: article.link });
                 } catch (err) {
