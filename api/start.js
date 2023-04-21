03fddd4ba456452827cada88c0f1004ecfc3151b
Lawrence Li <lonor@live.com>
2023-04-21 20:51:36 +0800

    log

--- api/start.js
+++ api/start.js
@@ -1,6 +1,7 @@
 const handleRSS = require('../main');
 
 module.exports = async (req, res) => {
+  console.log(req.headers["x-forwarded-for"]);
   const list = await handleRSS();
-  res.status(200).json(list)
+  res.status(200).json(list);
 }
