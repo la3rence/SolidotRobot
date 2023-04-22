cf288e5f80e4548a08f042d9cae969c3c4ba598c
Lawrence Li <lonor@live.com>
2023-04-22 17:33:02 +0800

    fix: typo

--- api/start.js
+++ api/start.js
@@ -7,7 +7,7 @@ const timeout = 1000 * 60 * 30;
 module.exports = async (req, res) => {
   const ip = req.headers["x-forwarded-for"];
   const ua = req.headers["user-agent"];
-  const timestamp = Data.now() - timeout;
+  const timestamp = Date.now() - timeout;
   if (history[ip] > timestamp || history[ua] > timestamp) {
     console.info(`Rate limit exceeded - ${ip} ${ua}`);
     res.status(200).json([]);
