f9931a2322e2ab225e5ca5bcdc67e3924010c3a9
Lawrence Li <lonor@live.com>
2023-04-22 17:31:52 +0800

    feat: add ua rate limit

--- api/start.js
+++ api/start.js
@@ -2,15 +2,20 @@ const handleRSS = require('../main');
 
 // in-memory rate limit
 const history = {};
+const timeout = 1000 * 60 * 30;
 
 module.exports = async (req, res) => {
   const ip = req.headers["x-forwarded-for"];
-  if (history[ip] > Date.now() - 1000 * 60 * 20){
-    console.warn(`Rate limit exceeded - ${ip}: ${history[ip]}`);
+  const ua = req.headers["user-agent"];
+  const timestamp = Data.now() - timeout;
+  if (history[ip] > timestamp || history[ua] > timestamp) {
+    console.info(`Rate limit exceeded - ${ip} ${ua}`);
     res.status(200).json([]);
     return;
   }
-  history[ip] = Date.now();
+  const now = Date.now();
+  history[ip] = now;
+  history[ua] = now;
   const list = await handleRSS();
   res.status(200).json(list);
 }
