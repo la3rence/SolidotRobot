5d78e3cd164778726130035929ce533d52c1eb1a
Lawrence Li <lonor@live.com>
2023-04-22 14:24:56 +0800

    feat: rate limit

--- api/start.js
+++ api/start.js
@@ -1,7 +1,16 @@
 const handleRSS = require('../main');
 
+// in-memory rate limit
+const history = {};
+
 module.exports = async (req, res) => {
-  console.log(req.headers["x-forwarded-for"]);
+  const ip = req.headers["x-forwarded-for"];
+  if (history[ip] > Date.now() - 1000 * 60 * 10){
+    console.warn(`Rate limit exceeded - ${ip}: ${history[ip]}`);
+    res.status(200).list([]);
+    return;
+  }
+  history[ip] = Date.now();
   const list = await handleRSS();
   res.status(200).json(list);
 }
