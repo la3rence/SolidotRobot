d14fec1b772df2d9fde0e640fc22e86adf991625
Lawrence Li <lonor@live.com>
2023-04-22 14:40:42 +0800

    feat: update limit window to 20 min

--- api/start.js
+++ api/start.js
@@ -5,7 +5,7 @@ const history = {};
 
 module.exports = async (req, res) => {
   const ip = req.headers["x-forwarded-for"];
-  if (history[ip] > Date.now() - 1000 * 60 * 10){
+  if (history[ip] > Date.now() - 1000 * 60 * 20){
     console.warn(`Rate limit exceeded - ${ip}: ${history[ip]}`);
     res.status(200).json([]);
     return;
